8b1a9630425015fe641e5f4b26dbdee7fbaf9301
Nanozuki Crows <nanozuki.crows@gmail.com>
2022-01-20 19:00:39 +0800

    explore setting

--- lua/tabby/v2/option.lua
+++ lua/tabby/v2/option.lua
@@ -1,3 +1,42 @@
-local option = {}
+local tabby = {}
+
+local option = {
+  layout = 'active_wins_at_tail',
+  hl = 'TabLineFill',
+  head = {
+    { '  ', hl = tabby.hl:from('TabLine') },
+    { '', hl = tabby.hl:from('TabLine'):sep('TabLineFill') },
+  },
+  tab = {
+    hl = tabby.hl:from('TabLine'),
+    sep = { left = '', right = '' },
+    label = { '', tabby.tab.number, tabby.tab.name, tabby.tab.close_btn('x') },
+    join = ' ',
+
+    active = {
+      hl = tabby.hl:from('TabLineSel'),
+      label = { '', tabby.tab.number, tabby.tab.name, tabby.tab.close_btn('x') },
+    },
+  },
+  win = {
+    hl = tabby.hl:from('TabLine'),
+    label = { '', tabby.win.name('unique') },
+    sep = { left = '', right = '' },
+    join = ' ',
+
+    top = {
+      label = { '', tabby.win.name('unique') },
+    },
+  },
+  tail = {
+    { '', hl = tabby.hl:from('TabLine'):sep('TabLineFill') },
+    { '  ', hl = tabby.hl:from('TabLine') },
+  },
+  ['tab.name'] = {
+    default = function(tabid)
+      tabby.win.unique_name(tabby.tab.focus_win(tabid))
+    end,
+  },
+}
 
 return option
