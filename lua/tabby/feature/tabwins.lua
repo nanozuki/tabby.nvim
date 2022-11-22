0c441b8e0833b037f1e267d1a708e21d42287144
crows <pt.wenhan@gmail.com>
2022-11-22 23:10:11 +0800

    fix: lua annotations

--- lua/tabby/feature/tabwins.lua
+++ lua/tabby/feature/tabwins.lua
@@ -53,7 +53,7 @@ end
 
 ---@class TabbyTabs
 ---@field tabs TabbyTab[] tabs
----@field foreach fun(fn:fun(tab:TabbyTab)):TabbyNode render tabs by given render function
+---@field foreach fun(fn:fun(tab:TabbyTab):TabbyNode):TabbyNode render tabs by given render function
 
 local function wrap_tab_node(node, tabid)
   if type(node) == 'string' then
@@ -127,7 +127,7 @@ end
 
 ---@class TabbyWins
 ---@field wins TabbyWin[] windows
----@field foreach fun(fn:fun(win:TabbyWin)):TabbyNode render wins by given render function
+---@field foreach fun(fn:fun(win:TabbyWin):TabbyNode):TabbyNode render wins by given render function
 
 ---@alias WinFilter fun(win:TabbyWin):boolean filter for window
 
