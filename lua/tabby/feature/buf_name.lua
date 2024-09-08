5bfa77c43a3677c24b402150a9464d541f09a648
Caps Lock <46106832+capsey@users.noreply.github.com>
2024-09-09 01:28:06 +0500

    feat!: add `buf.is_current()`, `buf.file_icon()` and `buf.name()`
    BREAKING: changes the API of `tabby.module.filename` and
    `tabby.feature.buf_name` to use bufid instead of winid

--- lua/tabby/feature/buf_name.lua
+++ lua/tabby/feature/buf_name.lua
@@ -18,19 +18,19 @@ end
 ---@param winid number
 ---@param opt? TabbyBufNameOption
 ---@return string
-function buf_name.get(winid, opt)
+function buf_name.get(bufid, opt)
   local o = default_option
   if opt ~= nil then
     o = vim.tbl_deep_extend('force', default_option, opt)
   end
   if o.mode == 'unique' then
-    return buf_name.get_unique_name(winid)
+    return buf_name.get_unique_name(bufid)
   elseif o.mode == 'relative' then
-    return buf_name.get_relative_name(winid)
+    return buf_name.get_relative_name(bufid)
   elseif o.mode == 'tail' then
-    return buf_name.get_tail_name(winid)
+    return buf_name.get_tail_name(bufid)
   elseif o.mode == 'shorten' then
-    return buf_name.get_shorten_name(winid)
+    return buf_name.get_shorten_name(bufid)
   else
     return ''
   end
@@ -38,26 +38,26 @@ end
 
 ---@param winid number
 ---@return string filename
-function buf_name.get_unique_name(winid)
-  return filename.unique(winid)
+function buf_name.get_unique_name(bufid)
+  return filename.unique(bufid)
 end
 
 ---@param winid number
 ---@return string filename
-function buf_name.get_relative_name(winid)
-  return filename.relative(winid)
+function buf_name.get_relative_name(bufid)
+  return filename.relative(bufid)
 end
 
 ---@param winid number
 ---@return string filename
-function buf_name.get_tail_name(winid)
-  return filename.tail(winid)
+function buf_name.get_tail_name(bufid)
+  return filename.tail(bufid)
 end
 
 ---@param winid number
 ---@return string filename
-function buf_name.get_shorten_name(winid)
-  return filename.shorten(winid)
+function buf_name.get_shorten_name(bufid)
+  return filename.shorten(bufid)
 end
 
 return buf_name
