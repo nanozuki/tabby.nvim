b00ef161387a22c70c32da3ce3bf574ec261aa9a
Nanozuki Crows <nanozuki.crows@gmail.com>
2024-12-16 11:02:44 +0800

    feat: make compatbility for module.filename

--- lua/tabby/module/filename.lua
+++ lua/tabby/module/filename.lua
@@ -1,33 +1,32 @@
-local M = {}
-
+local filename = {}
 local win_name = require('tabby.feature.win_name')
 
 ---@deprecated use require('tabby.feature.win_name').get(winid, { mode = 'relative' })
 ---@param winid number
 ---@return string filename
-function M.relative(winid)
+function filename.relative(winid)
   return win_name.get(winid, { mode = 'relative' })
 end
 
 ---@deprecated use require('tabby.feature.win_name').get(winid, { mode = 'tail' })
 ---@param winid number
 ---@return string filename
-function M.tail(winid)
+function filename.tail(winid)
   return win_name.get(winid, { mode = 'tail' })
 end
 
 ---@deprecated use require('tabby.feature.win_name').get(winid, { mode = 'shorten' })
 ---@param winid number
 ---@return string filename
-function M.shorten(winid)
+function filename.shorten(winid)
   return win_name.get(winid, { mode = 'shorten' })
 end
 
 ---@deprecated use require('tabby.feature.win_name').get(winid, { mode = 'unique' })
 ---@param winid number
 ---@return string filename
-function M.unique(winid)
+function filename.unique(winid)
   return win_name.get(winid, { mode = 'unique' })
 end
 
-return M
+return filename
