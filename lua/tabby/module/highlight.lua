071beb0cf96461cfb9e0a456515ba31477fd3e3f
Nanozuki Crows <nanozuki.crows@gmail.com>
2025-04-02 12:59:05 +0200

    feat: use v0.9 api to get/set highlight

--- lua/tabby/module/highlight.lua
+++ lua/tabby/module/highlight.lua
@@ -5,9 +5,56 @@ local highlight = {}
 ---@alias TabbyHighlightGroup string
 
 ---@class TabbyHighlightObject
----@field fg? string highlight argument guifg
----@field bg? string highlight argument guibg
+---@field fg? string|number highlight argument guifg
+---@field bg? string|number highlight argument guibg
 ---@field style? string highlight argument gui
+
+local attrlist = {
+  'bold',
+  'underline',
+  'undercurl',
+  'underdouble',
+  'underdotted',
+  'underdashed',
+  'strikethrough',
+  'reverse',
+  'italic',
+}
+
+---@param group_name string
+---@param hl TabbyHighlightObject
+local function set_hl(group_name, hl)
+  local val = {}
+  if hl.fg ~= nil then
+    val.fg = hl.fg
+  end
+  if hl.bg ~= nil then
+    val.bg = hl.bg
+  end
+  if hl.style ~= nil then
+    for _, k in ipairs(attrlist) do
+      if hl.style == k then
+        val[k] = true
+        break
+      end
+    end
+  end
+  vim.api.nvim_set_hl(0, group_name, val)
+end
+
+---@param group_name string
+---@return TabbyHighlightObject
+local function get_hl(group_name)
+  local nvim_hl = vim.api.nvim_get_hl(0, { name = group_name, link = false })
+  local hl = { fg = nvim_hl.fg, bg = nvim_hl.bg }
+  for _, k in ipairs(attrlist) do
+    if nvim_hl[k] then
+      hl.style = k
+      break
+    end
+  end
+  return hl
+end
 
 ---@type table<string, TabbyHighlightObject>
 local extract_cache = {}
@@ -25,41 +72,16 @@ vim.api.nvim_create_autocmd({ 'ColorScheme' }, {
 ---@param hl TabbyHighlightObject
 ---@return string highlight group name
 function highlight.register(hl)
-  vim.validate({
-    fg = { hl.fg, 'string', true },
-    bg = { hl.bg, 'string', true },
-    style = { hl.style, 'string', true },
-  })
-  local tabby_keys = { 'fg', 'bg', 'style' }
-  local nvim_keys = { 'guifg', 'guibg', 'gui' }
+  vim.validate('fg', hl.fg, { 'string', 'number' }, true)
+  vim.validate('bg', hl.bg, { 'string', 'number' }, true)
+  vim.validate('style', hl.style, 'string', true)
 
-  local groups = { 'TabbyHL' }
-  for _, k in ipairs(tabby_keys) do
-    groups[#groups + 1] = hl[k] or 'NONE'
-  end
+  local groups = { 'TabbyHL', hl.fg or 'NONE', hl.bg or 'NONE', hl.style or 'NONE' }
   local group = string.gsub(table.concat(groups, '_'), '#', '')
 
-  local cmds = { 'hi', group }
-  for i, k in ipairs(tabby_keys) do
-    if hl[k] ~= nil then
-      cmds[#cmds + 1] = string.format('%s=%s', nvim_keys[i], hl[k])
-    end
-  end
-  if #cmds == 2 then
-    cmds[#cmds + 1] = 'gui=NONE'
-  end
+  set_hl(group, hl)
   extract_cache[group] = hl
-  vim.cmd(table.concat(cmds, ' '))
   return group
-end
-
-local function get_hl_str(group_name)
-  if vim.fn.has('nvim-0.10') == 1 then
-    return vim.api.nvim_exec2('highlight ' .. group_name, { output = true }).output
-  else
-    ---@diagnostic disable-next-line: deprecated
-    return vim.api.nvim_exec('highlight ' .. group_name, true)
-  end
 end
 
 ---@param group_name string
@@ -68,23 +90,7 @@ function highlight.extract(group_name)
   if extract_cache[group_name] ~= nil then
     return extract_cache[group_name]
   end
-  local hl_str = get_hl_str(group_name)
-  local links_to = string.match(hl_str, 'links to (.*)$')
-  if links_to then
-    local hl = highlight.extract(links_to)
-    extract_cache[group_name] = hl
-    return hl
-  end
-  local hl = {}
-  for k, v in string.gmatch(hl_str, '([^%s=]+)=([^%s=]+)') do
-    if k == 'guifg' then
-      hl.fg = v
-    elseif k == 'guibg' then
-      hl.bg = v
-    elseif k == 'gui' then
-      hl.style = v
-    end
-  end
+  local hl = get_hl(group_name)
   extract_cache[group_name] = hl
   return hl
 end
